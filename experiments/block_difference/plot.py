169ee359806af067760a8502b8311345b9b47aec
v715 <vgopala4@jhu.edu>
2020-12-27 15:21:02 -0500

    Plot simulation results

---
+++ experiments/block_difference/plot.py
@@ -0,0 +1,98 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+# %%
+def get_method_name(row):
+    if row.average:
+        if row.binarize:
+            return "Average Connectivity"
+        else:
+            return "Average Edge Weight"
+    else:
+        if row.binarize:
+            return "Multivariate Binary"
+        else:
+            return "Multivariate Weighted"
+
+
+# %%
+df = pd.read_csv("results/block_simulation_2.csv")
+df["method"] = df.apply(get_method_name, axis="columns")
+df = df.drop(["binarize", "average"], axis="columns")
+df["reject"] = df["pvalue"] < 0.05
+df.head()
+
+# %%
+sns.lineplot(
+    data=df.query("distribution == 'same_mean'"),
+    x="sample_size",
+    y="reject",
+    hue="method",
+)
+plt.show()
+
+# %%
+fig, axs = plt.subplots(ncols=3, sharex=True, sharey=False, figsize=(8, 5))
+
+sns.lineplot(
+    data=df.query("distribution == 'equal'"),
+    x="sample_size",
+    y="reject",
+    hue="method",
+    legend=False,
+    ax=axs[0],
+)
+axs[0].set_box_aspect(1)
+axs[0].set(
+    xlabel="Sample Size",
+    ylabel="Recall",
+    title="Same Distribution",
+    ylim=(-0.05, 1.05),
+)
+
+sns.lineplot(
+    data=df.query("distribution == 'same_mean'"),
+    x="sample_size",
+    y="reject",
+    hue="method",
+    legend=True,
+    ax=axs[1],
+)
+axs[1].set_box_aspect(1)
+axs[1].set(
+    xlabel="Sample Size",
+    ylabel="Power",
+    title="Same Mean",
+    ylim=(-0.05, 1.05),
+)
+
+sns.lineplot(
+    data=df.query("distribution == 'diff_mean'"),
+    x="sample_size",
+    y="reject",
+    hue="method",
+    legend=False,
+    ax=axs[2],
+)
+axs[2].set_box_aspect(1)
+axs[2].set(
+    xlabel="Sample Size",
+    ylabel="Power",
+    title="Different Mean",
+    ylim=(-0.05, 1.05),
+)
+
+fig.tight_layout()
+axs[1].legend(
+    loc="upper center",
+    bbox_to_anchor=(0.5, -0.25),
+    fancybox=True,
+    shadow=True,
+    ncol=4,
+)
+plt.savefig("community_sim.pdf", dpi=300)
+
+# %%
